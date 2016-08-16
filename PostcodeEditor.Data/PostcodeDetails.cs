6f9ec27fe2d495e0a198d8def01d7b3b4ae2473a
Alastair Danley-Cox <alastair_cox@btinternet.com>
2016-08-16 07:48:15 +0100

    Added solution with web and data projects Tweaked colors temporarily for dev purposes Wrote first unit test for postcode controller

---
+++ PostcodeEditor.Data/PostcodeDetails.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PostcodeEditor.Data
+{
+    public class PostcodeDetails
+    {
+        object Postcode { get; set; }
+        object Latitude { get; set; }
+        object Longitude { get; set; }
+        object County{ get; set; }
+        object District { get; set; }
+        object Ward { get; set; }
+        object CountryRegion { get; set; }
+
+    }
+}
+
---
+++ PostcodeEditor.Tests/Controllers/PostcodeControllerTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using NUnit.Framework;
+using PostcodeEditor;
+using PostcodeEditor.Web.Controllers;
+
+namespace PostcodeEditor.Tests.Controllers
+{
+    [TestFixture]
+    public class PostcodeControllerTest
+    {
+        [Test]
+        public void Index()
+        {
+            
+            PostcodeController controller = new PostcodeController();
+            
+            ViewResult result = controller.Index() as ViewResult;
+            
+            Assert.IsNotNull(result);
+        }
+
+    }
+}
