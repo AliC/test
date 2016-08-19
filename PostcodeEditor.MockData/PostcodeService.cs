6445dd0a5de8bbdcb8a232babddb5f981410f455
Alastair Danley-Cox <alastair_cox@btinternet.com>
2016-08-19 08:09:08 +0100

    Hooked up csv sample file to mock data Removed bad line from csv sample that I assumed I created when I cut the original file down

--- PostcodeEditor.MockData/PostcodeService.cs
+++ PostcodeEditor.MockData/PostcodeService.cs
@@ -1,4 +1,7 @@
+using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using PostcodeEditor.SeparatedInterfaces;
 
 namespace PostcodeEditor.MockData
@@ -7,13 +10,41 @@ namespace PostcodeEditor.MockData
     {
         public IEnumerable<IPostcode> Get()
         {
-            yield return new PostcodeDetails
+            string[] lines = File.ReadAllLines(@"C:\dev\PostcodeEditor\PostcodeEditor.MockData\SampleCSV\postcodes.csv");
+            IEnumerable<IPostcode> postcodes = Parse(lines);
+
+            return postcodes;
+        }
+
+        private IEnumerable<IPostcode> Parse(string[] lines)
+        {
+            string[] headerRow = lines[0].Split(',');
+            IDictionary<string, int> index = new Dictionary<string, int>();
+
+            for(int i = 0; i < headerRow.Length; i++)
             {
-                Postcode = "QV1 1IJ",
-                CountryRegion = "From Mock Data"
-            };
+                if (headerRow[i] == "Postcode")
+                {
+                    index.Add("Postcode", i);
+                }
+                
+                //Note ADC: no CountryRegion header in sample csv file, so using Region instead
+                if (headerRow[i] == "Region")
+                {
+                    index.Add("Region", i);
+                }
+            }
+
+            foreach (string line in lines.Skip(1))
+            {
+                string[] columns = line.Split(',');
+
+                yield return new PostcodeDetails
+                {
+                    Postcode = columns[index["Postcode"]],
+                    CountryRegion = columns[index["Region"]],
+                };
+            }
+        }
     }
-
-
-}
 }
